b628911843b765ae3b07407c20b504f6bb8abacb
valerian.havaux <valerian.havaux@outlook.com>
2017-07-16 03:39:52 +0200

    clean exchanges component

--- src/modules/exchanges/exchanges.component.ts
+++ src/modules/exchanges/exchanges.component.ts
@@ -1,12 +1,4 @@
-import { YobitService } from './../../services/yobit/yobit.service';
-import { PoloniexService } from './../../services/poloniex/poloniex.service';
-import { BittrexTicker } from './../../models/bittrex/bittrex-ticker';
-import { BittrexCurrencies } from './../../models/bittrex/bittrex-currencies';
-import { BittrexMarkets } from './../../models/bittrex/bittrex-markets';
-import { BittrexService } from '../../services/bittrex/bittrex.service';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-exchanges',
@@ -14,37 +6,10 @@ import { Subject } from 'rxjs/Subject';
   styleUrls: ['./exchanges.component.css']
 })
 export class ExchangesComponent implements OnInit {
-  _poloniexMarketsNames: string[] = [];
-  _bittrexMarketsNames: string[] = [];
-  _yobitMarketsNames: string[] = [];
-  constructor(private _bittrexService: BittrexService,
-    private _poloniexService: PoloniexService,
-    private _yobitService: YobitService) { }
+
+  constructor() { }
 
   ngOnInit() {
-    this.getYobitMarketsNames();
-    this.getBittrexMarketsNames();
-    this.getPoloniexMarketsNames();
   }
-  getYobitMarketsNames() {
-    this._yobitService.getMarketsNames().subscribe(
-      data => this._yobitMarketsNames = data,
-      error => console.error(error)
-    );
-  }
-  getPoloniexMarketsNames() {
-    this._poloniexService.getMarketsNames().subscribe(
-      data => this._poloniexMarketsNames = data,
-      error => console.error(error)
-    );
-  }
-  getBittrexMarketsNames() {
-    this._bittrexService.getMarketsNames().subscribe(
-      data => this._bittrexMarketsNames = data,
-      error => console.error(error)
-    );
-  }
-  handleError(error) {
-    console.error(error);
-  }
+
 }
--- src/modules/exchanges/test-markets-names/test-markets-names.component.ts
+++ src/modules/exchanges/test-markets-names/test-markets-names.component.ts
@@ -1,3 +1,4 @@
+import { YobitService } from './../../../services/yobit/yobit.service';
 import { Component, OnInit } from '@angular/core';
 
 import { PoloniexService } from './../../../services/poloniex/poloniex.service';
@@ -9,19 +10,29 @@ import { BittrexService } from './../../../services/bittrex/bittrex.service';
   styleUrls: ['./test-markets-names.component.css']
 })
 export class TestMarketsNamesComponent implements OnInit {
-_poloniexMarketsNames: string[] = [];
+ _poloniexMarketsNames: string[] = [];
   _bittrexMarketsNames: string[] = [];
-  constructor(private _bittrexService: BittrexService, private _poloniexService: PoloniexService) { }
+  _yobitMarketsNames: string[] = [];
+  constructor(private _bittrexService: BittrexService,
+    private _poloniexService: PoloniexService,
+    private _yobitService: YobitService) { }
 
   ngOnInit() {
+    this.getYobitMarketsNames();
     this.getBittrexMarketsNames();
     this.getPoloniexMarketsNames();
+  }
+  getYobitMarketsNames() {
+    this._yobitService.getMarketsNames().subscribe(
+      data => this._yobitMarketsNames = data,
+      error => console.error(error)
+    );
   }
   getPoloniexMarketsNames() {
     this._poloniexService.getMarketsNames().subscribe(
       data => this._poloniexMarketsNames = data,
       error => console.error(error)
-    )
+    );
   }
   getBittrexMarketsNames() {
     this._bittrexService.getMarketsNames().subscribe(
